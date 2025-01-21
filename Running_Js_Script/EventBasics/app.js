3c1d3e3ce1864e9a7e4ae598903e2ceeff12daf9
JavaScriptFREE <wjsrhkdtn123@naver.com>
2025-01-21 17:07:16 +0900

    JS 8일차

--- Running_Js_Script/EventBasics/app.js
+++ Running_Js_Script/EventBasics/app.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('#v2');
+/* const btn = document.querySelector('#v2');
 btn.onclick = function () {
     console.log("YOU CLICKED ME!");
     console.log("I HOPE IT WORKED");
@@ -26,10 +26,41 @@ function shout() {
     console.log('SHOUT');
 }
 
-const tasButton = document.querySelector('#tas');
+const tasButton = document.querySelector('#tas'); */
 
 /* tasButton.onclick = twist;
 tasButton.onclick = shout; */
-
+/* 
 tasButton.addEventListener('click', twist, { once: true });
 tasButton.addEventListener('click', shout);
+ */
+
+/* const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const list = document.querySelector("#cats")
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const catName = input.value;
+    const newLI = document.createElement("LI");
+    newLI.innerText = catName;
+    list.append(newLI);
+    input.value = '';
+}); */
+
+const button = document.querySelector('#changeColor');
+const container = document.querySelector('#container');
+
+button.addEventListener('click', function (e) {
+    container.style.backgroundColor = makeRandomColor();
+    e.stopPropagation();
+})
+container.addEventListener('click', function () {
+    container.classList.toggle('hide');
+})
+
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
