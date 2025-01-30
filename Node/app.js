b6653a2dbbd067a0d068f0ca87cd70a45912dbb3
Noah0615 <wjsrhkdtn123@naver.com>
2025-01-30 22:23:04 +0900

    JS 13일차
    node는 맥북으로 진행하게 될 것 같다. 왜 window에서 node가 실행이 안되는 걸까?

--- Node/app.js
+++ Node/app.js
@@ -1,3 +1,6 @@
-/* const math = require('./math');
-console.log(math.PI); */
-console.log('Hello, world!');
+const math = require('./math');
+// console.log(math.PI);
+// console.log('Hello, world!');
+
+const cats = require('./shelter');
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats);
