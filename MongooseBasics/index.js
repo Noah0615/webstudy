f9e74c7bb7de6a7a8e6850054c08444506c0750e
Noah0615 <wjsrhkdtn123@naver.com>
2025-02-05 00:44:02 +0900

    MongoDB 2일차
    390. 섹션 주제
    
    391. Mongoose란?
    
    392. 몽구스 연결 문제 해결에 관한 참고 사항
    
    393. Mongo에 Mongoose를 연결하기
    
    394. node .load index.js 명령 문제에 관한 참고 사항
    
    395. 우리의 첫 번째 Mongoose 모델
    
    396. 대량 삽입하기
    
    397. Mongoose로 찾기
    
    398. Mongoose로 업데이트하기
    
    399. Mongoose로 삭제하기!
    
    400. Mongoose 스키마 유효성 검사
    
    401. 추가 스키마 제약 조건
    
    402. Mongoose 업데이트 유효성 검사하기
    
    403. Mongoose 유효성 검사 오류
    
    404. 인스턴스 메서드
    
    405. 정적 메서드 추가하기
    
    406. 가상 Mongoose
    
    407. Mongoose를 미들웨어로 정의하기

---
+++ MongooseBasics/index.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+    .then(() => {
+        console.log('Connection Open');
+    })
+    .catch(err => {
+        console.log('Error');
+        console.log(err);
+    });
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+// // const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+
+// Movie.insertMany([
+//     { title: 'The Shawshank Redemption', year: 1994, score: 9.3, rating: 'R' },
+//     { title: 'The Godfather', year: 1972, score: 9.2, rating: 'R' },
+//     { title: 'The Dark Knight', year: 2008, score: 9.0, rating: 'PG-13' },
+//     { title: 'Pulp Fiction', year: 1994, score: 8.9, rating: 'R' },
+//     { title: 'Schindler\'s List', year: 1993, score: 8.9, rating: 'R' }
+// ])
+//     .then(data => {
+//         console.log("IT WORKED");
+//         console.log(data);
+//     })
+
+async function removeDuplicates() {
+    try {
+        const movies = await Movie.find({});
+        const uniqueMovies = new Map();
+
+        movies.forEach(movie => {
+            const key = `${movie.title}-${movie.year}-${movie.score}-${movie.rating}`;
+            if (uniqueMovies.has(key)) {
+                uniqueMovies.get(key).push(movie._id); // 여기에서 오류를 수정합니다.
+            } else {
+                uniqueMovies.set(key, [movie._id]);
+            }
+        });
+
+        for (let [key, ids] of uniqueMovies) {
+            if (ids.length > 1) {
+                const idsToDelete = ids.slice(1);
+                await Movie.deleteMany({ _id: { $in: idsToDelete } });
+            }
+        }
+        console.log("중복된 영화 문서가 제거되었습니다.");
+    } catch (err) {
+        console.log("중복 제거 중 오류가 발생했습니다:", err);
+    }
+}
+
+// removeDuplicates();
